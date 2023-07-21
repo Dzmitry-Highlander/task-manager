3a2edf3d1ef0edb7a91d39c8771fa60427e39219
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-21 11:55:53 +0300

    add: User.java

---
+++ user-service/src/main/java/by/itacademy/jd2/user_service/dao/entity/User.java
@@ -0,0 +1,44 @@
+package by.itacademy.jd2.user_service.dao.entity;
+
+import by.itacademy.jd2.user_service.core.enums.EUserRole;
+import by.itacademy.jd2.user_service.core.enums.EUserStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users", schema = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class User implements Serializable {
+    @Id
+    @Column(name = "user_id")
+    private UUID id;
+    @CreationTimestamp
+    @Column(name = "dt_create")
+    private LocalDateTime createDate;
+    @Version
+    @UpdateTimestamp
+    @Column(name = "dt_update")
+    private LocalDateTime updateDate;
+    @Column(name = "mail")
+    private String mail;
+    @Column(name = "fio")
+    private String fio;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private EUserRole role;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private EUserStatus status;
+    @Column(name = "password")
+    private String password;
+}
