3ffa329b320497e6fcc6064a0f92f4b0ee0ff946
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-21 12:37:49 +0300

    add: IUserService.java

---
+++ user-service/src/main/java/by/itacademy/jd2/user_service/service/api/IUserService.java
@@ -0,0 +1,7 @@
+package by.itacademy.jd2.user_service.service.api;
+
+import by.itacademy.jd2.user_service.core.dto.UserCreateDTO;
+import by.itacademy.jd2.user_service.dao.entity.User;
+
+public interface IUserService extends ICRUDService<User, UserCreateDTO> {
+}
