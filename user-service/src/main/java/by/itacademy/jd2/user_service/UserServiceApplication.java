58571976e390dd89772498d41c4cf499aec37e31
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-21 13:01:16 +0300

    refactor: UserServiceApplication.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/UserServiceApplication.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/UserServiceApplication.java
@@ -2,7 +2,11 @@ package by.itacademy.jd2.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableJpaRepositories(basePackages = "by.itacademy.jd2.user_service.dao.api")
+@EnableTransactionManagement
 @SpringBootApplication
 public class UserServiceApplication {
 
