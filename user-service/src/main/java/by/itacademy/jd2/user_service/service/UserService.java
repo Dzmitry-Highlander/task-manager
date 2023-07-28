9aef2338c9ae3b33f0dd42202abc2968e1855e93
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-28 19:23:16 +0300

    refactor: UserController.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/service/UserService.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/service/UserService.java
@@ -18,6 +18,7 @@ public class UserService implements IUserService {
 
     @Override
     public User create(UserCreateDTO item) {
+        //TODO проверка email в базе или нет?
         return userDao.save(Objects.requireNonNull(conversionService.convert(item, User.class)));
     }
 
