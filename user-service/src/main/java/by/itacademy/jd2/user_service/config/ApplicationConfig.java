62d241110fe09682a9ec453b007f3dbe64f3f744
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-01 01:16:50 +0300

    refactor: ApplicationConfig.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/config/ApplicationConfig.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/config/ApplicationConfig.java
@@ -25,6 +25,8 @@ public class ApplicationConfig {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 
         authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
 
+        return authProvider;
     }
 }
