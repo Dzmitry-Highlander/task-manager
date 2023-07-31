5d43b39b768f7ef01a61c7996dc5cd8aee94f092
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-01 01:16:09 +0300

    refactor: ApplicationConfig.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/config/ApplicationConfig.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/config/ApplicationConfig.java
@@ -4,6 +4,8 @@ import by.itacademy.jd2.user_service.dao.api.IUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -16,5 +18,13 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> repository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found!")); //TODO Exception
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+
     }
 }
