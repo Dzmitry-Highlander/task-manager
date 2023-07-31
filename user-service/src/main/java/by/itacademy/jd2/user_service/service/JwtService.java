98919fe42430cf2797feaf2ac0b39cddb2c94f5c
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-31 23:32:59 +0300

    refactor: JwtService.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/service/JwtService.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/service/JwtService.java
@@ -44,6 +44,20 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean validate(String token, UserDetails userDetails) {
+        final String userEmail = extractUserEmail(token);
+
+        return (userEmail.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
