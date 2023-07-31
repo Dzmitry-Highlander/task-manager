f2da244c25af1c7353fa8f02d51ea0738d00f457
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-31 23:52:51 +0300

    add: SecurityConfig.java

---
+++ user-service/src/main/java/by/itacademy/jd2/user_service/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package by.itacademy.jd2.user_service.config;
+
+import by.itacademy.jd2.user_service.dao.api.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final IUserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+    }
+}
