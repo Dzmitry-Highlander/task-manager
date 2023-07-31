094495032856c0c0c2d1cc11356823411e05e195
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-31 22:42:44 +0300

    refactor: JwtAuthFilter.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/config/JwtAuthFilter.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/config/JwtAuthFilter.java
@@ -5,11 +5,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
+import static org.apache.logging.log4j.util.Strings.isEmpty;
 
 @Component
 @RequiredArgsConstructor
@@ -20,6 +23,12 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String JWTToken;
 
+        if (isEmpty(authHeader) || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
     }
 }
