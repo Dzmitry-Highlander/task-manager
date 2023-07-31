4897ea639a096da2d15d847f766023f8e997c0ce
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-31 23:19:07 +0300

    refactor: JwtService.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/service/JwtService.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/service/JwtService.java
@@ -5,12 +5,20 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Function;
+
 @Service
 public class JwtService {
     private JWTProperty property;
 
     public String extractUserEmail(String jwt) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
