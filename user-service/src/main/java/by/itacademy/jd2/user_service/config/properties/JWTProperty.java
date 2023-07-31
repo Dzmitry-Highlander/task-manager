3e6741548facbfdb5604e576ca512b63139f05b6
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-31 20:50:35 +0300

    refactor: JWTProperty.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/config/properties/JWTProperty.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/config/properties/JWTProperty.java
@@ -1,4 +1,13 @@
 package by.itacademy.jd2.user_service.config.properties;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "jwt")
 public class JWTProperty {
+    private String secret;
+    private String issuer;
 }
