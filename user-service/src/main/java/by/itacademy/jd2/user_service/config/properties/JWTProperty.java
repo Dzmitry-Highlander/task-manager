48527124d1eeae3e7638d07df53b759b4ebe5580
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-31 23:12:32 +0300

    refactor: application.yml, JWTProperty.java, JwtService.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/config/properties/JWTProperty.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/config/properties/JWTProperty.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.security.Key;
+
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "jwt")
 public class JWTProperty {
-    private String secret;
+    private Key key;
     private String issuer;
 }
--- user-service/src/main/java/by/itacademy/jd2/user_service/service/JwtService.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/service/JwtService.java
@@ -1,11 +1,14 @@
 package by.itacademy.jd2.user_service.service;
 
+import by.itacademy.jd2.user_service.config.properties.JWTProperty;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+    private JWTProperty property;
+
     public String extractUserEmail(String jwt) {
         return null;
     }
@@ -13,7 +16,7 @@ public class JwtService {
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
-                .setSigningKey(getSigningKey())
+                .setSigningKey(property.getSecret())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
