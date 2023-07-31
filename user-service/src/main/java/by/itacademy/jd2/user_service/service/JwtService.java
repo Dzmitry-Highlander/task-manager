41a8126e9f72e340730bbf6a66380e90a803adab
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-31 23:25:55 +0300

    refactor: JwtService.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/service/JwtService.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/service/JwtService.java
@@ -3,13 +3,20 @@ package by.itacademy.jd2.user_service.service;
 import by.itacademy.jd2.user_service.config.properties.JWTProperty;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 @Service
+@AllArgsConstructor
 public class JwtService {
-    private JWTProperty property;
+    private final JWTProperty property;
 
     public String extractUserEmail(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -19,6 +26,20 @@ public class JwtService {
         final Claims claims = extractAllClaims(token);
 
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(7)))
+                .signWith(property.getKey(), SignatureAlgorithm.ES256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
