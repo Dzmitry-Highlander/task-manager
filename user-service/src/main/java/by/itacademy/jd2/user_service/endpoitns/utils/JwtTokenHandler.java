d562e3d7ec2f0fd6ebe8a1a2ee341f212e889d78
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-07-31 21:14:22 +0300

    refactor: JwtTokenHandler.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/endpoitns/utils/JwtTokenHandler.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/endpoitns/utils/JwtTokenHandler.java
@@ -1,20 +1,19 @@
 package by.itacademy.jd2.user_service.endpoitns.utils;
 
 import by.itacademy.jd2.user_service.config.properties.JWTProperty;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+@AllArgsConstructor
 @Component
 public class JwtTokenHandler {
     private final JWTProperty property;
-
-    public JwtTokenHandler(JWTProperty property) {
-        this.property = property;
-    }
 
     public String generateAccessToken(UserDetails user) {
         return generateAccessToken(user.getUsername());
@@ -28,43 +27,5 @@ public class JwtTokenHandler {
                 .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(7)))
                 .signWith(SignatureAlgorithm.HS512, property.getSecret())
                 .compact();
-    }
-
-    public String getUsername(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(property.getSecret())
-                .parseClaimsJws(token)
-                .getBody();
-
-        return claims.getSubject();
-    }
-
-    public Date getExpirationDate(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(property.getSecret())
-                .parseClaimsJws(token)
-                .getBody();
-
-        return claims.getExpiration();
-    }
-
-    public boolean validate(String token) {
-        try {
-            Jwts.parser().setSigningKey(property.getSecret()).parseClaimsJws(token);
-
-            return true;
-        } catch (SignatureException ex) {
-            //logger.error("Invalid JWT signature - {}", ex.getMessage());
-        } catch (MalformedJwtException ex) {
-            //logger.error("Invalid JWT token - {}", ex.getMessage());
-        } catch (ExpiredJwtException ex) {
-            //logger.error("Expired JWT token - {}", ex.getMessage());
-        } catch (UnsupportedJwtException ex) {
-            //logger.error("Unsupported JWT token - {}", ex.getMessage());
-        } catch (IllegalArgumentException ex) {
-            //logger.error("JWT claims string is empty - {}", ex.getMessage());
-        }
-
-        return false;
     }
 }
