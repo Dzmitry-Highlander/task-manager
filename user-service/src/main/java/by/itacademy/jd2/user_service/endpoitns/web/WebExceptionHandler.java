f2a370a4d8523677e29a2283e5beb4df35ac2ff2
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-02 02:32:20 +0300

    add: WebExceptionHandler.java

---
+++ user-service/src/main/java/by/itacademy/jd2/user_service/endpoitns/web/WebExceptionHandler.java
@@ -0,0 +1,19 @@
+package by.itacademy.jd2.user_service.endpoitns.web;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class WebExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = {IllegalStateException.class})
+    protected ResponseEntity<Object> handleIllegalStateConflict(
+            RuntimeException exception, WebRequest request
+    ) {
+        String bodyOfResponse = exception.getMessage();
+
+        return handleExceptionInternal(exception, bodyOfResponse, new HttpHeaders(), HttpStatus.I_AM_A_TEAPOT, request);
+    }
+}
