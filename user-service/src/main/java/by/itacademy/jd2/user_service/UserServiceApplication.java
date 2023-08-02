580bb4172d6da9488c03cab490f47639976c8981
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-02 12:53:12 +0300

    refactor: UserServiceApplication.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/UserServiceApplication.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/UserServiceApplication.java
@@ -1,10 +1,14 @@
 package by.itacademy.jd2.user_service;
 
+import by.itacademy.jd2.user_service.config.properties.JWTProperty;
+import by.itacademy.jd2.user_service.config.properties.MailProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableConfigurationProperties({MailProperty.class, JWTProperty.class})
 @EnableJpaRepositories(basePackages = "by.itacademy.jd2.user_service.dao.api")
 @EnableTransactionManagement
 @SpringBootApplication
