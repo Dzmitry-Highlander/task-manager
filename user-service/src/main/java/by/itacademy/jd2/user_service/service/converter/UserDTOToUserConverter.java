9249f718738664eb4b76ff7ebf086ef23e8fd2bf
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-03 10:20:39 +0300

    add: ActivatorDTOtoActivatorConverter.java, UserDTOToUserConverter.java

---
+++ user-service/src/main/java/by/itacademy/jd2/user_service/service/converter/UserDTOToUserConverter.java
@@ -0,0 +1,27 @@
+package by.itacademy.jd2.user_service.service.converter;
+
+import by.itacademy.jd2.user_service.core.dto.UserDTO;
+import by.itacademy.jd2.user_service.dao.entity.User;
+import org.springframework.core.convert.converter.Converter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+public class UserDTOToUserConverter implements Converter<UserDTO, User> {
+    @Override
+    public User convert(UserDTO source) {
+        return User.builder()
+                .uuid(source.getId())
+                .createDate(LocalDateTime.ofInstant(Instant.ofEpochMilli(source.getCreateDate()),
+                        TimeZone.getDefault().toZoneId()))
+                .updateDate(LocalDateTime.ofInstant(Instant.ofEpochMilli(source.getUpdateDate()),
+                        TimeZone.getDefault().toZoneId()))
+                .email(source.getEmail())
+                .fio(source.getFio())
+                .role(source.getRole())
+                .status(source.getStatus())
+                .build();
+    }
+}
