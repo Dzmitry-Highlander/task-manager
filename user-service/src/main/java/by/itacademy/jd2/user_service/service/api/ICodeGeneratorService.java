327ffd9c6eef492b50a97c73fccc6df914b0415a
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-03 11:29:16 +0300

    del: CodeGeneratorService.java, ICodeGeneratorService.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/service/api/ICodeGeneratorService.java
+++
@@ -1,8 +0,0 @@
-package by.itacademy.jd2.user_service.service.api;
-
-import by.itacademy.jd2.user_service.core.dto.ActivatorDTO;
-import by.itacademy.jd2.user_service.core.dto.UserDTO;
-
-public interface ICodeGeneratorService {
-    ActivatorDTO generate(UserDTO userDTO);
-}
