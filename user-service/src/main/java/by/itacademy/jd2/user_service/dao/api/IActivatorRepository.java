6e95db990b06173ea5f55d1a0c5521d4c47f8350
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-03 10:32:43 +0300

    add: IActivatorRepository.java, IAuthenticationService.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/dao/api/IActivatorRepository.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/dao/api/IActivatorRepository.java
@@ -1,9 +1,12 @@
 package by.itacademy.jd2.user_service.dao.api;
 
 import by.itacademy.jd2.user_service.dao.entity.Activator;
+import by.itacademy.jd2.user_service.dao.entity.User;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IActivatorRepository extends ListCrudRepository<Activator, UUID> {
+    Optional<Activator> findByEmail(String email);
 }
--- user-service/src/main/java/by/itacademy/jd2/user_service/service/api/IAuthenticationService.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/service/api/IAuthenticationService.java
@@ -1,4 +1,13 @@
 package by.itacademy.jd2.user_service.service.api;
 
+import by.itacademy.jd2.user_service.core.dto.AuthenticationResponseDTO;
+import by.itacademy.jd2.user_service.core.dto.UserLoginDTO;
+import by.itacademy.jd2.user_service.core.dto.UserRegistrationDTO;
+
 public interface IAuthenticationService {
+    AuthenticationResponseDTO register(UserRegistrationDTO request);
+
+    AuthenticationResponseDTO login(UserLoginDTO request);
+
+    String verification(String code, String mail);
 }
