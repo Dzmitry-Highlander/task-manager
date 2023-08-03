20fb20248bbc1846463f5298df09d471f4db8086
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-04 02:47:24 +0300

    refactor: AuthenticationController.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/endpoitns/web/AuthenticationController.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/endpoitns/web/AuthenticationController.java
@@ -1,45 +1,40 @@
 package by.itacademy.jd2.user_service.endpoitns.web;
 
-import by.itacademy.jd2.user_service.core.dto.UserDTO;
 import by.itacademy.jd2.user_service.core.dto.UserLoginDTO;
 import by.itacademy.jd2.user_service.core.dto.UserRegistrationDTO;
-import by.itacademy.jd2.user_service.core.dto.AuthenticationResponseDTO;
-import by.itacademy.jd2.user_service.dao.api.IUserRepository;
-import by.itacademy.jd2.user_service.dao.entity.User;
 import by.itacademy.jd2.user_service.service.api.IAuthenticationService;
 import by.itacademy.jd2.user_service.service.api.IUserHolder;
-import by.itacademy.jd2.user_service.service.exception.AuthException;
+import by.itacademy.jd2.user_service.service.api.IUserService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/users")
 @AllArgsConstructor
 public class AuthenticationController {
-    private final ConversionService conversionService;
-    private final IUserRepository userRepository;
+    private static final String USER_REGISTERED = "Пользователь зарегистрирован";
     private final IAuthenticationService authenticationService;
+    private final IUserService userService;
     private final IUserHolder userHolder;
 
     @PostMapping("/registration")
-    public ResponseEntity<AuthenticationResponseDTO> register(
+    public ResponseEntity<?> register(
             @RequestBody @Valid UserRegistrationDTO request
     ) {
-        return ResponseEntity.ok(authenticationService.register(request));
+        return new ResponseEntity<>(USER_REGISTERED, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponseDTO> register(
+    public ResponseEntity<?> register(
             @RequestBody @Valid UserLoginDTO request
     ) {
-        return ResponseEntity.ok(authenticationService.login(request));
+        var response = authenticationService.login(request);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/registration/confirm")
@@ -47,18 +42,16 @@ public class AuthenticationController {
             @RequestParam("code") String code,
             @RequestParam("email") @Email String email
     ) {
-        return ResponseEntity.ok(authenticationService.verification(code, email));
+        var response = authenticationService.verification(code, email);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/me")
     public ResponseEntity<?> me() {
-        Optional<User> userOptional = userRepository
+        var response = userService
                 .findByEmail(userHolder.getUser().getUsername());
-        UserDTO userDTO =  conversionService.convert(
-                userOptional.orElseThrow(
-                        () -> new AuthException("Access denied!")), UserDTO.class
-        );
 
-        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
