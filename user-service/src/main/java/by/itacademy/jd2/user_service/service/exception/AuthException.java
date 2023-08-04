6a054adc61a0f140f367f1dd83bf2730146faef1
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-04 19:16:51 +0300

    delete: AuthException.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/service/exception/AuthException.java
+++
@@ -1,9 +0,0 @@
-package by.itacademy.jd2.user_service.service.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class AuthException extends AuthenticationException {
-    public AuthException(String msg) {
-        super(msg);
-    }
-}
