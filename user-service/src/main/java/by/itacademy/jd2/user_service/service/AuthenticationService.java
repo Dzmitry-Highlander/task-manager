d8f3d7386c0902885e4c9c3d605f6dc99d509cbf
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-04 19:17:00 +0300

    refactor: AuthenticationService.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/service/AuthenticationService.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/service/AuthenticationService.java
@@ -6,12 +6,7 @@ import by.itacademy.jd2.user_service.core.enums.EUserStatus;
 import by.itacademy.jd2.user_service.dao.api.IActivatorRepository;
 import by.itacademy.jd2.user_service.dao.entity.Activator;
 import by.itacademy.jd2.user_service.dao.entity.User;
-import by.itacademy.jd2.user_service.service.api.IAuthenticationService;
-import by.itacademy.jd2.user_service.service.api.IMailSenderService;
-import by.itacademy.jd2.user_service.service.api.IUserService;
-import by.itacademy.jd2.user_service.service.exception.AuthException;
-import by.itacademy.jd2.user_service.service.exception.EmailAlreadyTakenException;
-import by.itacademy.jd2.user_service.service.util.CodeGenerator;
+import by.itacademy.jd2.user_service.service.api.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,7 +16,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Objects;
+import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -40,10 +38,14 @@ AuthenticationService implements IAuthenticationService {
     private final IUserService userService;
     private final IActivatorRepository activatorRepository;
     private final IMailSenderService mailSenderService;
+    private final IUserHolder userHolder;
+    private final ICodeGeneratorService codeGeneratorService;
 
     @Override
-    public AuthenticationResponseDTO register(UserRegistrationDTO request)   {
-        var code = CodeGenerator.generate();
+    @Transactional
+    public void register(UserRegistrationDTO request)   {
+        var time = System.currentTimeMillis();
+        var code = codeGeneratorService.generate();
         var user = UserCreateDTO.builder()
                 .fio(request.getFio())
                 .email(request.getEmail())
@@ -51,20 +53,26 @@ AuthenticationService implements IAuthenticationService {
                 .role(EUserRole.ROLE_USER)
                 .status(EUserStatus.WAITING_ACTIVATION)
                 .build();
-        var activator = ActivatorCreateDTO.builder()
+        var activator = Activator.builder()
                     .email(request.getEmail())
                     .code(code)
-                    .createDate(System.currentTimeMillis())
-                    .expirationDate(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(30))
+                    .createDate(
+                            LocalDateTime.ofInstant(Instant.ofEpochMilli(time),
+                                    TimeZone.getDefault().toZoneId())
+                    )
+                    .expirationDate(
+                            LocalDateTime.ofInstant(Instant.ofEpochMilli(time + TimeUnit.MINUTES.toMillis(30)),
+                                    TimeZone.getDefault().toZoneId())
+                    )
                     .build();
 
-        userService.create(user);
-        activatorRepository.save(Objects.requireNonNull(conversionService.convert(activator, Activator.class)));
+        activatorRepository.save(Objects.requireNonNull(activator));
         mailSenderService.send(code, request.getEmail());
+        userService.create(user);
 
         var jwtToken = jwtService.generateToken(conversionService.convert(user, User.class));
 
-        return AuthenticationResponseDTO.builder()
+        AuthenticationResponseDTO.builder()
                 .token(jwtToken)
                 .build();
     }
@@ -79,7 +87,7 @@ AuthenticationService implements IAuthenticationService {
                     )
             );
         } catch (AuthenticationException ex) {
-            throw new AuthException(EMAIL_OR_PASSWORD_ERROR);
+            throw new IllegalStateException(EMAIL_OR_PASSWORD_ERROR);
         }
 
         var user = userService.findByEmail(request.getEmail());
@@ -94,7 +102,7 @@ AuthenticationService implements IAuthenticationService {
     @Transactional
     public String verification(String code, String email) {
         Activator activator = activatorRepository.findByEmail(email)
-                .orElseThrow(() -> new AuthException(VERIFICATION_FAILED));
+                .orElseThrow(() -> new IllegalStateException(VERIFICATION_FAILED));
         UserDTO user = userService.findByEmail(email);
 
         if (Objects.equals(code, activator.getCode())) {
@@ -102,16 +110,17 @@ AuthenticationService implements IAuthenticationService {
                     .role(user.getRole())
                     .build();
 
+            //TODO отдельный метод на активацию пользователя
             userService.update(user.getId(), user.getUpdateDate(), userUpdate);
 
             return VERIFICATION_SUCCESS;
+        } else {
+            throw new IllegalStateException(VERIFICATION_FAILED);
         }
-
-        return VERIFICATION_FAILED;
     }
 
     @Override
-    public UserDTO me(String email) {
-        return userService.findByEmail(email);
+    public UserDTO me() {
+        return userService.findByEmail(userHolder.getUser().getUsername());
     }
 }
