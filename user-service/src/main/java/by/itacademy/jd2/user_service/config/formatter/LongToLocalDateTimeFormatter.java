9de85d31aff9723f28ce1ec4a852f935e36c99b6
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-06 13:32:53 +0300

    refactor: IAuthenticationService.java, IUserService.java, LongToLocalDateTimeFormatter.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/config/formatter/LongToLocalDateTimeFormatter.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/config/formatter/LongToLocalDateTimeFormatter.java
@@ -1,4 +1,40 @@
 package by.itacademy.jd2.user_service.config.formatter;
 
-public class LongToLocalDateTimeFormatter {
+import by.itacademy.jd2.user_service.core.dto.ErrorResponseDTO;
+import by.itacademy.jd2.user_service.core.enums.EErrorType;
+import org.springframework.format.Formatter;
+import org.springframework.lang.NonNull;
+
+import java.text.ParseException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class LongToLocalDateTimeFormatter implements Formatter<LocalDateTime> {
+    private static final String NOT_CORRECT_INPUT_ERROR = "Введены некорректные символы, допустимы только цифры";
+
+    @NonNull
+    @Override
+    public LocalDateTime parse(@NonNull String text, @NonNull Locale locale) throws ParseException {
+        try {
+            long milliseconds = Long.parseLong(text);
+
+            return LocalDateTime.ofInstant(Instant.ofEpochMilli(milliseconds), ZoneId.systemDefault());
+        } catch (NumberFormatException e) {
+            List<ErrorResponseDTO> errorResponseDTOS = new ArrayList<>();
+
+            errorResponseDTOS.add(new ErrorResponseDTO(EErrorType.ERROR, NOT_CORRECT_INPUT_ERROR));
+
+            throw new RuntimeException();
+        }
+    }
+
+    @NonNull
+    @Override
+    public String print(@NonNull LocalDateTime object, @NonNull Locale locale) {
+        return object.toString();
+    }
 }
--- user-service/src/main/java/by/itacademy/jd2/user_service/service/api/IUserService.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/service/api/IUserService.java
@@ -1,7 +1,6 @@
 package by.itacademy.jd2.user_service.service.api;
 
 import by.itacademy.jd2.user_service.core.dto.UserCreateDTO;
-import by.itacademy.jd2.user_service.core.dto.UserDTO;
 import by.itacademy.jd2.user_service.dao.entity.User;
 
 public interface IUserService extends ICRUDService<User, UserCreateDTO> {
