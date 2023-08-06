d540d52949e05fc97aa9b6922af0852e1056cd06
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-06 22:27:51 +0300

    refactor: AuditController.java

--- audit-service/src/main/java/by/itacademy/jd2/audit_service/endpoint/web/AuditController.java
+++ audit-service/src/main/java/by/itacademy/jd2/audit_service/endpoint/web/AuditController.java
@@ -2,18 +2,15 @@ package by.itacademy.jd2.audit_service.endpoint.web;
 
 import by.itacademy.jd.base_pakage.core.dto.PageDTO;
 import by.itacademy.jd2.audit_service.core.dto.AuditDTO;
-import by.itacademy.jd2.audit_service.dao.entity.Audit;
 import by.itacademy.jd2.audit_service.service.api.IAuditService;
 import by.itacademy.jd2.audit_service.service.converter.PageToPageDTOConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.ConversionService;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/audit")
@@ -26,9 +23,15 @@ public class AuditController {
     public ResponseEntity<PageDTO<AuditDTO>> list(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size) {
-        Page<Audit> audits = this.auditService.get(page, size);
-        PageDTO<AuditDTO> pageDTO = PageToPageDTOConverter.convert(audits, conversionService);
+        PageDTO<AuditDTO> pageDTO = PageToPageDTOConverter.convert(auditService.get(page, size), conversionService);
 
         return new ResponseEntity<>(pageDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/audit/{uuid}")
+    public ResponseEntity<AuditDTO> findById(@PathVariable UUID uuid){
+        AuditDTO dto = conversionService.convert(auditService.get(uuid), AuditDTO.class);
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 }
