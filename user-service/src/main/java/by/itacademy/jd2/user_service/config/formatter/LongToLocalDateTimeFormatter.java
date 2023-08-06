9d35bc93db5bc68f035991ede0e8c57ac6f7d7e6
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-06 13:34:17 +0300

    refactor: LongToLocalDateTimeFormatter.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/config/formatter/LongToLocalDateTimeFormatter.java
+++ user-service/src/main/java/by/itacademy/jd2/user_service/config/formatter/LongToLocalDateTimeFormatter.java
@@ -1,7 +1,5 @@
 package by.itacademy.jd2.user_service.config.formatter;
 
-import by.itacademy.jd2.user_service.core.dto.ErrorResponseDTO;
-import by.itacademy.jd2.user_service.core.enums.EErrorType;
 import org.springframework.format.Formatter;
 import org.springframework.lang.NonNull;
 
@@ -9,8 +7,6 @@ import java.text.ParseException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 public class LongToLocalDateTimeFormatter implements Formatter<LocalDateTime> {
@@ -24,11 +20,7 @@ public class LongToLocalDateTimeFormatter implements Formatter<LocalDateTime> {
 
             return LocalDateTime.ofInstant(Instant.ofEpochMilli(milliseconds), ZoneId.systemDefault());
         } catch (NumberFormatException e) {
-            List<ErrorResponseDTO> errorResponseDTOS = new ArrayList<>();
-
-            errorResponseDTOS.add(new ErrorResponseDTO(EErrorType.ERROR, NOT_CORRECT_INPUT_ERROR));
-
-            throw new RuntimeException();
+            throw new IllegalArgumentException(NOT_CORRECT_INPUT_ERROR);
         }
     }
 
