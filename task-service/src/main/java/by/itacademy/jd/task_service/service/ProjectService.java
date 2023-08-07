6df17c8e7b9d86846e59284e623fc8c12725ac3a
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-07 20:47:33 +0300

    refactor: ProjectService.java

--- task-service/src/main/java/by/itacademy/jd/task_service/service/ProjectService.java
+++ task-service/src/main/java/by/itacademy/jd/task_service/service/ProjectService.java
@@ -1,14 +1,19 @@
 package by.itacademy.jd.task_service.service;
 
 import by.itacademy.jd.task_service.core.dto.ProjectCreateDTO;
+import by.itacademy.jd.task_service.core.dto.UserRefDTO;
 import by.itacademy.jd.task_service.dao.api.IProjectRepository;
 import by.itacademy.jd.task_service.dao.entity.Project;
 import by.itacademy.jd.task_service.service.api.IProjectService;
+import by.itacademy.jd.task_service.service.exception.ItemNotFoundException;
+import by.itacademy.jd.task_service.service.exception.VersionsNotMatchException;
 import lombok.AllArgsConstructor;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -16,6 +21,9 @@ import java.util.UUID;
 @Service
 @AllArgsConstructor
 public class ProjectService implements IProjectService {
+    private static final String PROJECT_NOT_FOUND_ERROR = "Проект с таким uuid не найден";
+    private static final String VERSIONS_NOT_MATCH_ERROR = "Версии не совпадают";
+
     private final IProjectRepository projectRepository;
     private final ConversionService conversionService;
 
@@ -35,12 +43,32 @@ public class ProjectService implements IProjectService {
     @Override
     public Project read(UUID uuid) {
         return projectRepository.findById(uuid)
-                //TODO Custom Exception
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(() -> new ItemNotFoundException(PROJECT_NOT_FOUND_ERROR));
     }
 
+    @Transactional
     @Override
     public Project update(UUID uuid, Long version, ProjectCreateDTO item) {
-        return null;
+        Project project = projectRepository.findById(uuid)
+                .orElseThrow(() -> new ItemNotFoundException(PROJECT_NOT_FOUND_ERROR));
+
+        if (!version.equals(project.getUpdateDate().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli())) {
+            throw new VersionsNotMatchException(VERSIONS_NOT_MATCH_ERROR);
+        }
+
+        List<UserRefDTO> stuff = item.getStaff();
+        List<UUID> uuids = new ArrayList<>();
+
+        for (UserRefDTO users : stuff) {
+            uuids.add(users.getUuid());
+        }
+
+        project.setName(item.getName());
+        project.setDescription(item.getDescription());
+        project.setManager(item.getManager().getUuid());
+        project.setStuff(uuids);
+        project.setStatus(item.getStatus());
+
+        return projectRepository.save(project);
     }
 }
