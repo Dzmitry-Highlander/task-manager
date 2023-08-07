e5fbd56cdc6c10f02aa37dce9652120c092f3f5f
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-07 21:04:18 +0300

    add: TaskSaveDTOToTaskConverter.java

---
+++ task-service/src/main/java/by/itacademy/jd/task_service/service/converter/TaskSaveDTOToTaskConverter.java
@@ -0,0 +1,17 @@
+package by.itacademy.jd.task_service.service.converter;
+
+import by.itacademy.jd.task_service.core.dto.TaskSaveDTO;
+import by.itacademy.jd.task_service.dao.entity.Task;
+import org.springframework.core.convert.converter.Converter;
+
+public class TaskSaveDTOToTaskConverter implements Converter<TaskSaveDTO, Task> {
+    @Override
+    public Task convert(TaskSaveDTO source) {
+        return Task.builder()
+                .title(source.getTitle())
+                .description(source.getDescription())
+                .implementer(source.getImplementer().getUuid())
+                .project(source.getProject().getUuid())
+                .build();
+    }
+}
