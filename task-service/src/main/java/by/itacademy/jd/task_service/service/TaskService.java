19ebbe84b27801e78fa96eaec18f5ed57f4f6ff1
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-09 15:29:43 +0300

    refactor: TaskService.java

--- task-service/src/main/java/by/itacademy/jd/task_service/service/TaskService.java
+++ task-service/src/main/java/by/itacademy/jd/task_service/service/TaskService.java
@@ -5,11 +5,13 @@ import by.itacademy.jd.task_service.dao.api.ITaskRepository;
 import by.itacademy.jd.task_service.dao.entity.Task;
 import by.itacademy.jd.task_service.service.api.ITaskService;
 import by.itacademy.jd.task_service.service.exception.ItemNotFoundException;
+import by.itacademy.jd.task_service.service.exception.VersionsNotMatchException;
 import lombok.AllArgsConstructor;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -45,6 +47,19 @@ public class TaskService implements ITaskService {
     @Transactional
     @Override
     public Task update(UUID uuid, Long version, TaskSaveDTO item) {
-        return null;
+        Task task = taskRepository.findById(uuid)
+                .orElseThrow(() -> new ItemNotFoundException(TASK_NOT_FOUND_ERROR));
+
+        if (!version.equals(task.getUpdateDate().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli())) {
+            throw new VersionsNotMatchException(VERSIONS_NOT_MATCH_ERROR);
+        }
+
+        task.setTitle(item.getTitle());
+        task.setDescription(item.getDescription());
+        task.setImplementer(item.getImplementer().getUuid());
+        task.setProject(item.getProject().getUuid());
+        task.setStatus(item.getStatus());
+
+        return taskRepository.save(task);
     }
 }
