572746340bc3efdc9ef9a5097c071a8e30682e71
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-12 22:00:50 +0300

    add: IUserInteractService.java, UserInteractService.java

---
+++ audit-service/src/main/java/by/itacademy/jd2/audit_service/service/UserInteractService.java
@@ -0,0 +1,18 @@
+package by.itacademy.jd2.audit_service.service;
+
+import by.itacademy.jd2.audit_service.service.api.IUserInteractService;
+import by.itacademy.jd2.audit_service.service.feign.UserServiceClient;
+import by.itacademy.jd2.base_package.core.dto.UserShortDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserInteractService implements IUserInteractService {
+    private final UserServiceClient userServiceClient;
+
+    @Override
+    public UserShortDTO sendAndGet(String bearerToken) {
+        return userServiceClient.send("Bearer " + bearerToken).getBody();
+    }
+}
