53d2717f6711d9668be819f0341df8de6c3e0806
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-13 11:59:03 +0300

    add: IUserServiceClient.java

---
+++ audit-service/src/main/java/by/itacademy/jd2/audit_service/service/feign/IUserServiceClient.java
@@ -0,0 +1,15 @@
+package by.itacademy.jd2.audit_service.service.feign;
+
+import by.itacademy.jd2.base_package.core.dto.UserShortDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(value = "user-service", url = "http://user-service:8080/users/me")
+public interface IUserServiceClient {
+
+    @GetMapping
+    ResponseEntity<UserShortDTO> send(@RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken);
+}
