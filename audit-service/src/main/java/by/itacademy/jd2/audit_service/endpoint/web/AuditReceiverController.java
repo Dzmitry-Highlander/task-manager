f0889ee20b7804e6dc845daa24c6d0db4e30e593
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-13 11:56:35 +0300

    refactor: AuditReceiverController.java

--- audit-service/src/main/java/by/itacademy/jd2/audit_service/endpoint/web/AuditReceiverController.java
+++ audit-service/src/main/java/by/itacademy/jd2/audit_service/endpoint/web/AuditReceiverController.java
@@ -2,7 +2,6 @@ package by.itacademy.jd2.audit_service.endpoint.web;
 
 import by.itacademy.jd2.audit_service.service.api.IAuditReceiverService;
 import by.itacademy.jd2.base_package.core.dto.AuditCreateDTO;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Validated
-@RequestMapping("/audit")
+@RequestMapping("/internal/audit")
 @RequiredArgsConstructor
 public class AuditReceiverController {
     private final IAuditReceiverService auditReceiverService;
