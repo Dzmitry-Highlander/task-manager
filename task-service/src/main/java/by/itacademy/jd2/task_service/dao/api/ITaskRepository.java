a50dae6929166880537c85f8c279f3c599debf5e
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-15 21:50:44 +0300

    refactor: ITaskRepository.java

--- task-service/src/main/java/by/itacademy/jd2/task_service/dao/api/ITaskRepository.java
+++ task-service/src/main/java/by/itacademy/jd2/task_service/dao/api/ITaskRepository.java
@@ -3,7 +3,6 @@ package by.itacademy.jd2.task_service.dao.api;
 import by.itacademy.jd2.task_service.dao.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.UUID;
 
