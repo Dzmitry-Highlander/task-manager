689b33e7b06b6305195135bdc53efd36a856ae32
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-15 21:51:59 +0300

    add: IUserService.java

---
+++ audit-service/src/main/java/by/itacademy/jd2/audit_service/service/api/IUserService.java
@@ -0,0 +1,7 @@
+package by.itacademy.jd2.audit_service.service.api;
+
+import by.itacademy.jd2.base_package.core.dto.UserShortDTO;
+
+public interface IUserService {
+    UserShortDTO getMe(String jwt);
+}
