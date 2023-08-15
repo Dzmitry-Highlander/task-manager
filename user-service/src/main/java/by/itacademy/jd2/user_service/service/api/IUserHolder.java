f027d3906a21147686b22b28b3f19760c44229d1
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-15 21:43:15 +0300

    delete: UserHolder.java, IUserHolder.java

--- user-service/src/main/java/by/itacademy/jd2/user_service/service/api/IUserHolder.java
+++
@@ -1,7 +0,0 @@
-package by.itacademy.jd2.user_service.service.api;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface IUserHolder {
-    UserDetails getUser();
-}
