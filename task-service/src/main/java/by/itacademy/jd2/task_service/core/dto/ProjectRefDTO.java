fb57a9a7a68a2864ffc09bd354c4b10ea1735614
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-19 23:07:33 +0300

    add: ProjectRefDTO.java

---
+++ task-service/src/main/java/by/itacademy/jd2/task_service/core/dto/ProjectRefDTO.java
@@ -0,0 +1,16 @@
+package by.itacademy.jd2.task_service.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectRefDTO {
+    public UUID uuid;
+}
