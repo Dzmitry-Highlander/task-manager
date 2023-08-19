d9bce017f9a10ecfb161c838cb0ba501f267bb4f
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-19 23:44:16 +0300

    refactor: TaskCreationDTO.java

--- task-service/src/main/java/by/itacademy/jd2/task_service/core/dto/create/TaskCreationDTO.java
+++ task-service/src/main/java/by/itacademy/jd2/task_service/core/dto/create/TaskCreationDTO.java
@@ -2,7 +2,6 @@ package by.itacademy.jd2.task_service.core.dto.create;
 
 import by.itacademy.jd2.task_service.core.dto.ref.ProjectRefDTO;
 import by.itacademy.jd2.task_service.core.dto.ref.UserRefDTO;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -18,12 +17,10 @@ public class TaskCreationDTO {
     @NotNull
     private ProjectRefDTO project;
 
-    @NotBlank
     @Size(min = 3, max = 20)
     private String title;
 
-    @NotBlank
-    @Size(max = 255)
+    @Size(min = 5, max = 255)
     private String description;
 
     @NotNull
