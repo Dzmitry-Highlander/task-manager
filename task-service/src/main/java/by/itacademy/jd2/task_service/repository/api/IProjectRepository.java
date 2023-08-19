26178cfb902d4614cc7b4d0236e8e7f4454e1d78
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-20 02:06:59 +0300

    refactor: IProjectRepository.java, ITaskRepository.java

--- task-service/src/main/java/by/itacademy/jd2/task_service/repository/api/IProjectRepository.java
+++ task-service/src/main/java/by/itacademy/jd2/task_service/repository/api/IProjectRepository.java
@@ -2,8 +2,10 @@ package by.itacademy.jd2.task_service.repository.api;
 
 import by.itacademy.jd2.task_service.repository.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface IProjectRepository extends JpaRepository<Project, UUID> {
 }
--- task-service/src/main/java/by/itacademy/jd2/task_service/repository/api/ITaskRepository.java
+++ task-service/src/main/java/by/itacademy/jd2/task_service/repository/api/ITaskRepository.java
@@ -2,8 +2,11 @@ package by.itacademy.jd2.task_service.repository.api;
 
 import by.itacademy.jd2.task_service.repository.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ITaskRepository extends JpaRepository<Task, UUID> {
+
 }
