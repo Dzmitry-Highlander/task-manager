3c6b03d54ba8bd7dd6102d5cecbb2394cf6dbc82
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-08-19 23:28:37 +0300

    add: TaskCreationDTO.java

---
+++ task-service/src/main/java/by/itacademy/jd2/task_service/core/dto/create/TaskCreationDTO.java
@@ -0,0 +1,23 @@
+package by.itacademy.jd2.task_service.core.dto.create;
+
+import by.itacademy.jd2.task_service.core.dto.ref.ProjectRefDTO;
+import by.itacademy.jd2.task_service.core.dto.ref.UserRefDTO;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class TaskCreationDTO {
+    @NotNull
+    private ProjectRefDTO project;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String title;
+
+    @NotBlank
+    @Size(max = 255)
+    private String description;
+
+    @NotNull
+    private UserRefDTO implementer;
+}
